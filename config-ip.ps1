f7573b22ded8a391112f1fa96b37055285de76b6
Julien Marcucci <julien.marcucci@mediaschool.me>
2025-05-23 17:00:11 +0200

    Ajout du script config-ip.ps1

---
+++ config-ip.ps1
@@ -0,0 +1,16 @@
+﻿# Demande à l'utilisateur les infos réseau
+$ip = Read-Host "Entrez l'adresse IP"
+$mask = Read-Host "Entrez le masque (ex: 24)"
+$gateway = Read-Host "Entrez la passerelle"
+$dns = Read-Host "Entrez l'adresse du DNS"
+
+# Récupère la carte réseau connectée
+$interface = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
+
+# Applique l'adresse IP et la passerelle
+New-NetIPAddress -InterfaceAlias $interface.Name -IPAddress $ip -PrefixLength $mask -DefaultGateway $gateway
+
+# Configure l'adresse DNS
+Set-DnsClientServerAddress -InterfaceAlias $interface.Name -ServerAddresses $dns
+
+Write-Host "Configuration IP appliquée avec succès."
