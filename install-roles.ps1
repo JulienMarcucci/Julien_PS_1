bf79adf1cf22f4648df764f776652e166a39ffd1
Julien Marcucci <julien.marcucci@mediaschool.me>
2025-05-23 17:39:29 +0200

    Ajout du script install-roles.ps1

---
+++ install-roles.ps1
@@ -0,0 +1,12 @@
+﻿# Liste des rôles à installer
+$roles = @(
+    "AD-Domain-Services",
+    "DNS",
+    "DHCP"
+)
+
+# Installation
+Install-WindowsFeature -Name $roles -IncludeManagementTools
+
+# Confirmation
+Write-Host "Les rôles ADDS, DNS et DHCP ont été installés avec succès." -ForegroundColor Green
