8c5e17e43fbd4ed976efd0b9ccae325a53118995
Julien Marcucci <julien.marcucci@mediaschool.me>
2025-06-06 14:22:53 +0200

    configure-adds.ps1

---
+++ configure-adds.ps1
@@ -0,0 +1,8 @@
+﻿# configure-adds.ps1
+
+$domainName = Read-Host "Entrez le nom de domaine (ex: formation.local)"
+$secureDSRMPwd = Read-Host "Entrez le mot de passe DSRM" -AsSecureString
+
+Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword $secureDSRMPwd -CreateDnsDelegation $false -DatabasePath "C:\Windows\NTDS" -DomainMode WinThreshold -DomainNetbiosName ($domainName.Split('.')[0].ToUpper()) -ForestMode WinThreshold -InstallDns $true -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -NoRebootOnCompletion $false -Force
+
+Read-Host "AD DS configuré. Appuyez sur Entrée pour fermer."
