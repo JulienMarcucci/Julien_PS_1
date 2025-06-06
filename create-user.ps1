b692562ee7645b0ffe11584ee0c6afcc94ad93fb
Julien Marcucci <julien.marcucci@mediaschool.me>
2025-06-06 15:23:28 +0200

    create-user.ps1

---
+++ create-user.ps1
@@ -0,0 +1,21 @@
+﻿# create-user.ps1
+
+# Demande les informations nécessaires
+$firstName = Read-Host "Prénom de l'utilisateur"
+$lastName = Read-Host "Nom de l'utilisateur"
+$userName = "$firstName.$lastName"
+$password = Read-Host "Mot de passe pour l'utilisateur (sera converti en SecureString)" -AsSecureString
+$ouPath = Read-Host "Chemin LDAP de l'OU (ex: OU=Informatique,DC=formation,DC=local)"
+
+# Création du compte utilisateur
+New-ADUser `
+    -Name "$firstName $lastName" `
+    -GivenName $firstName `
+    -Surname $lastName `
+    -SamAccountName $userName `
+    -UserPrincipalName "$userName@formation.local" `
+    -AccountPassword $password `
+    -Path $ouPath `
+    -Enabled $true
+
+Write-Host "✅ Utilisateur '$userName' créé dans '$ouPath'."
