3e390db772ea2843e7d565c46409df29871486c5
Julien Marcucci <julien.marcucci@mediaschool.me>
2025-06-13 13:37:20 +0200

    Mise à jour du script d'import d'utilisateurs

---
+++ import-users.ps1
@@ -0,0 +1,26 @@
+﻿# import-users.ps1
+
+# Chemin vers le fichier CSV
+$csvPath = Read-Host "Entrez le chemin complet du fichier CSV (ex: C:\Users\Administrateur\Desktop\utilisateurs.csv)"
+
+
+# Nettoyer la variable en enlevant les espaces inutiles
+$csvPath = $csvPath.Trim()
+
+# Importation des utilisateurs
+Import-Csv -Path $csvPath | ForEach-Object {
+    $securePass = ConvertTo-SecureString $_.Password -AsPlainText -Force
+    $upn = "$($_.Username)@formation.local"
+
+    New-ADUser `
+        -Name "$($_.FirstName) $($_.LastName)" `
+        -GivenName $_.FirstName `
+        -Surname $_.LastName `
+        -SamAccountName $_.Username `
+        -UserPrincipalName $upn `
+        -Path $_.OU `
+        -AccountPassword $securePass `
+        -Enabled $true
+
+    Write-Host "✅ Utilisateur '$($_.Username)' importé avec succès."
+}
